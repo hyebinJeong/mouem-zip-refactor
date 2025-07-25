906fcd304c1947337ea12feb185a79004d6343e4
Gyeongjo <119joe@naver.com>
2025-07-25 15:06:14 +0900

    feat: 통합 응답 DTO 추가

---
+++ backend/src/main/java/org/scoula/register/domain/dto/RegisterAnalysisResponse.java
@@ -0,0 +1,18 @@
+package org.scoula.register.domain.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RegisterAnalysisResponse {
+    private List<List<String>> tableData;
+    private List<MortgageDTO> mortgageInfos;                                // 근저당권
+    private List<SeizureDTO> seizureInfos;                                  // 압류
+    private List<ProvisionalSeizureDTO> provisionalSeizureInfos;            // 가압류
+    private List<AuctionDTO> auctionInfos;                                  // 경매
+    private List<ProvisionalRegistrationDTO> provisionalRegistrationInfos;  // 가등기
+    private List<InjunctionDTO> injunctionInfos;                            // 가처분
+    private List<JeonseRightDTO> jeonseRightInfos;                          // 전세권설정
+    private List<TrustDTO> trustInfos;                                      // 신탁등기
+}

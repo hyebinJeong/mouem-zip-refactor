29a48553a90a0fe9446de4c4644c77aa50c90518
LSY0108 <116865290+LSY0108@users.noreply.github.com>
2025-07-28 11:24:57 +0900

    refactor: Update LeaseRequestDTO.java
    isApt 제거

--- backend/src/main/java/org/scoula/jeonseRate/dto/LeaseRequestDTO.java
+++ backend/src/main/java/org/scoula/jeonseRate/dto/LeaseRequestDTO.java
@@ -13,5 +13,4 @@ import lombok.ToString;
 public class LeaseRequestDTO {
     private String address;         // 사용자가 입력한 주소
     private String jeonsePrice;     // 전세 보증금 (단위: 만원)
-    private String isApt;           // 아파트 여부 ("Y" 또는 "N" 등으로 사용) - 유형 분류에 사용됨
 }

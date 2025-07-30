4c1232342097c42c68de2b3de9d8e6b4790a40cf
dragon-6 <joysong2002@naver.com>
2025-07-30 15:11:26 +0900

    카테고리 백엔드(미완성)

--- backend/src/main/java/org/scoula/categorymanager/domain/CategorymanagerVO.java
+++ backend/src/main/java/org/scoula/categorymanager/domain/CategorymanagerVO.java
@@ -6,35 +6,28 @@ public class CategorymanagerVO {
     private String description;
     private String categoryColor;
 
-    // Getters & Setters
+    // Getters and Setters
     public int getCategoryId() {
         return categoryId;
     }
-
     public void setCategoryId(int categoryId) {
         this.categoryId = categoryId;
     }
-
     public String getCategoryName() {
         return categoryName;
     }
-
     public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public String getCategoryColor() {
         return categoryColor;
     }
-
     public void setCategoryColor(String categoryColor) {
         this.categoryColor = categoryColor;
     }

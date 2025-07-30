c1167add78d4a916f436fcb392f7946b42e7e53e
hyebinJeong <beeni1211@naver.com>
2025-07-30 17:41:17 +0900

    feat: 체크리스트 컴포넌트 추가

--- frontend/src/components/final-report/FinalChecklist.vue
+++ frontend/src/components/final-report/FinalChecklist.vue
@@ -5,15 +5,32 @@ import { computed } from 'vue';
 const props = defineProps({
   checked: { type: Array, required: true },
 });
+
+const uncheckedItems = computed(() => {
+  return checklistStore.filter((_, index) => !props.checked?.[index]);
+});
 </script>
 
 <template>
   <div class="FinalChecklist">
-    <h2>체크리스트 컴포넌트</h2>
+    <div
+      v-for="(item, index) in uncheckedItems"
+      :key="index"
+      class="checkedlist-box p-3 rounded mb-3 text-start"
+    >
+      <p class="mb-0" v-html="item" />
+    </div>
   </div>
 </template>
 
 <style scoped>
-/* .FinalChecklist {
-} */
+.FinalChecklist {
+  width: fit-content;
+  margin: 0 auto;
+}
+
+.checkedlist-box {
+  background-color: #fffcd4;
+  width: 45vw;
+}
 </style>

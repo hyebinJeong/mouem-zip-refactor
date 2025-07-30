370166587e7e49a0473e070551d55ab832fecab2
Gyeongjo <119joe@naver.com>
2025-07-30 15:03:17 +0900

    feat: userId로 등기부등본 찾기 쿼리 추가

--- backend/src/main/java/org/scoula/register/mapper/RegisterMapper.java
+++ backend/src/main/java/org/scoula/register/mapper/RegisterMapper.java
@@ -4,8 +4,13 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.scoula.register.domain.dto.RegisterDTO;
 
+import java.util.List;
+
 @Mapper
 public interface RegisterMapper {
     void insertRegister(RegisterDTO register);
     RegisterDTO selectByRegisterId(Integer registerId);
+
+    // 사용자별 등기부 분석 목록 조회
+    List<RegisterDTO> selectByUserId(Integer userId);
 }

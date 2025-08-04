fb647325b37ad5a8cc42410444ff791aadd5878c
LSY0108 <seungye1209@gmail.com>
2025-08-04 16:35:12 +0900

    feat: RegisterResult.vue UI 수정

--- frontend/src/pages/RegisterResult.vue
+++ frontend/src/pages/RegisterResult.vue
@@ -55,7 +55,7 @@
             <span v-else style="color: gray">판단 불가</span>
           </p>
           <hr class="my-3" />
-          <p class="fw-bold fs-5 mb-2" style="color: #151fae">주의 사항</p>
+          <p class="fw-bold fs-5 mb-1" style="color: #151fae">주의 사항</p>
           <div class="analysis-cards-wrapper">
             <AnalysisCards
               v-if="result && result.analysis"

5a79d150752e593288ad58798288554bc0f60f05
LSY0108 <seungye1209@gmail.com>
2025-12-26 09:28:49 +0900

    chore(k6): 사용하지 않는 k6 테스트 시나리오 정리

--- k6/scripts/run_load_scenario.ps1
+++
@@ -1,9 +0,0 @@
-k6 run `
-  -e BASE_URL=http://localhost:8080 `
-  -e USER_ID=1 `
-  -e WARMUP_CREATE=0 `
-  -e CHECKLIST_POST_RATE=0.01 `
-  C:/Users/seung/Desktop/Project/mouem-zip-refactor/k6/scenarios/load-scenario.js
-
-
-#  .\run_load_scenario.ps1 명령어로 실행
